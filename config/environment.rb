25de2553a365aa7a3a08cf74d1defb3cb21053d4
HarshTrivedi <harshjtrivedi94@gmail.com>
2014-01-13 00:50:49 -0500

    configure sendgrid in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ThoughtApp::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'app21034641@heroku.com',
+  :password => 'rq9vjtvj',
+  :domain => 'http://thoughtapp.herokuapp.com/',
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
